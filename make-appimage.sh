d704fbe99ac15fe7580e0ba2b7c9f3797404cd7c
Link4Electronics <link4electronics@gmail.com>
2026-03-10 01:53:53 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,7 +9,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/512x512/apps/net.86box.86Box.png
 export DESKTOP=/usr/share/applications/net.86box.86Box.desktop
-export STARTUPWMCLASS=
+export STARTUPWMCLASS=net.86box.86Box
 export DEPLOY_QT=1
 
 # Deploy dependencies
